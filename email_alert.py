d2269421f72d9ae4fa75a9ceb3179bd17a7847ac
Sean Hong <hongsean1@gmail.com>
2023-01-28 11:30:50 -0500

    added .env to work on authentication

--- email_alert.py
+++ email_alert.py
@@ -1,5 +1,10 @@
 from email.message import EmailMessage
+from os import getenv
 from smtplib import SMTP_SSL
+
+from dotenv import load_dotenv
+
+####################################################################################################
 
 # create the email message
 def create_email_message(subject: str, recipient: str, receiver: str, message: str) -> EmailMessage:
@@ -14,11 +19,11 @@ def create_email_message(subject: str, recipient: str, receiver: str, message: s
     return email_msg
 
 # send the email
-def send_email(email_msg: EmailMessage) -> None:
+def send_email(email: str, password: str, email_msg: EmailMessage) -> None:
     protocol = SMTP_SSL("smtp.gmail.com", 465)
 
     try:
-        protocol.login("", "")
+        protocol.login(email, password)
         protocol.send_message(email_msg)
     except Exception as e:
         print(e)
@@ -27,8 +32,16 @@ def send_email(email_msg: EmailMessage) -> None:
 
 # main method
 def main():
+    # load environment variables
+    load_dotenv()
+
+    # get the email address and password for authentication
+    email = getenv("EMAIL")
+    password = getenv("PASSWORD")
+
+    # create the email message and send it
     email_msg = create_email_message("", "", "", "")
-    send_email(email_msg)
+    send_email(email, password, email_msg)
 
 # execute main method
 if __name__ == "__main__":
