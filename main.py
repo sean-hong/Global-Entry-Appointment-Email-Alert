eba098421f7496d033b7f5d79da84b5fdbdf81aa
Sean Hong <hongsean1@gmail.com>
2023-01-28 13:53:30 -0500

    renamed variable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from os import getenv
-
 from dotenv import load_dotenv
-
 from email_alert import create_email_message, send_email
 from global_entry_api import call_api, read_api_data
 
@@ -13,10 +11,10 @@ def main():
     # get the environment stuff
     env_email = getenv("EMAIL")
     env_password = getenv("PASSWORD")
-    location_id = int(getenv("LOCATION"))
+    env_location_id = int(getenv("LOCATION"))
 
     # get the soonest appointment info from the airport
-    appointment_info = call_api(location_id)
+    appointment_info = call_api(env_location_id)
 
     # make sure that the appointment is available
     if appointment_info is not None:
@@ -24,7 +22,7 @@ def main():
         subject = "Global Entry Appointment"
         sender = env_email
         receiver = env_email
-        message = read_api_data(location_id, appointment_info)
+        message = read_api_data(env_location_id, appointment_info)
 
         # create the email message and send it
         email_msg = create_email_message(subject, sender, receiver, message)
