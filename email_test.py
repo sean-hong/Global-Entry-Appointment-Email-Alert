51563819db9dd994b3212e18e3fd0e3ab7c093ad
Sean Hong <hongsean1@gmail.com>
2023-01-28 11:12:33 -0500

    added prior work

---
+++ email_test.py
@@ -0,0 +1,35 @@
+from email.message import EmailMessage
+from smtplib import SMTP_SSL
+
+# create the email message
+def create_email_message(recipient: str, receiver: str, subject: str, message: str) -> EmailMessage:
+    email_msg = EmailMessage()
+
+    email_msg["Subject"] = subject
+    email_msg["From"] = recipient
+    email_msg["To"] = receiver
+
+    email_msg.set_content(message)
+
+    return email_msg
+
+# send the email
+def send_email(email_msg: str) -> None:
+    protocol = SMTP_SSL("smtp.gmail.com", 465)
+
+    try:
+        protocol.login("", "")
+        protocol.send_message(email_msg)
+    except Exception as e:
+        print(e)
+    finally:
+        protocol.quit()
+
+# main method
+def main():
+    email_msg = create_email_message("", "", "", "")
+    send_email(email_msg)
+
+# execute main method
+if __name__ == "__main__":
+    main()
