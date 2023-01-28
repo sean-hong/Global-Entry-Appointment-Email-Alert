4ddcfcf095465308d79b5c94cddd1f3b9ece8b6d
Sean Hong <hongsean1@gmail.com>
2023-01-28 13:45:37 -0500

    complete main method

--- main.py
+++ main.py
@@ -3,25 +3,36 @@ from os import getenv
 from dotenv import load_dotenv
 
 from email_alert import create_email_message, send_email
+from global_entry_api import call_api, read_api_data
 
 # main method
 def main():
     # load environment variables
     load_dotenv()
 
-    # get the email address and password for authentication
+    # get the environment stuff
     env_email = getenv("EMAIL")
     env_password = getenv("PASSWORD")
+    location_id = int(getenv("LOCATION"))
 
-    # set the email parameters
-    subject = ""
-    recipient = ""
-    receiver = ""
-    message = ""
+    # get the soonest appointment info from the airport
+    appointment_info = call_api(location_id)
 
-    # create the email message and send it
-    email_msg = create_email_message("", "", "", "")
-    send_email(env_email, env_password, email_msg)
+    # make sure that the appointment is available
+    if appointment_info is not None:
+        # set the email parameters
+        subject = "Global Entry Appointment"
+        sender = env_email
+        receiver = env_email
+        message = read_api_data(location_id, appointment_info)
+
+        # create the email message and send it
+        email_msg = create_email_message(subject, sender, receiver, message)
+        send_email(env_email, env_password, email_msg)
+
+        print("Email sent")
+    else:
+        print("No appointment spots available")
 
 # execute main method
 if __name__ == "__main__":
