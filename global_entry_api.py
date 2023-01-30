3ace4dac9bcf7cd01a1febe7ae1605c6c370ed3c
Sean Hong <hongsean1@gmail.com>
2023-01-29 23:41:50 -0500

    return typing edit

--- global_entry_api.py
+++ global_entry_api.py
@@ -11,7 +11,7 @@ def get_airport(location: int) -> str:
     return airports.get(location)
 
 # call the API
-def call_api(location: int) -> dict:
+def call_api(location: int) -> dict | None:
     api_url = f"https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=1&locationId={location}&minimum=1"
 
     try:
